d00839cee9d26b7c7a89bc63f1a23f7ab1274403
Zbyszko <bylo12345@gmail.com>
2023-10-31 17:24:46 +0100

    Updated experience and skills

--- src/constants/languages/en.ts
+++ src/constants/languages/en.ts
@@ -1,25 +1,24 @@
-import { ITranslations } from "../../interfaces/languages";
-
+import { ITranslations } from '../../interfaces/languages'
 export const translations: ITranslations = {
     overlay: {
-        download: "Download CV",
-        changeLanguage: "Change language:",
+        download: 'Download CV',
+        changeLanguage: 'Change language:',
     },
     about: {
-        title: "Who am I?",
+        title: 'Who am I?',
         description:
-            "I am an ambitious student looking for new challenges. I am interested in electronics, psychology, game design and video editing. Additionally, I am a very communicative and open person, which is why I enjoy traveling, meeting new people and broadening my horizons. I love analyzing problems and solving them in the best, most optimal way.",
+            'I am an ambitious student looking for new challenges. I am interested in electronics, psychology, game design and video editing. Additionally, I am a very communicative and open person, which is why I enjoy traveling, meeting new people and broadening my horizons. I love analyzing problems and solving them in the best, most optimal way.',
     },
     contact: {
-        title: "Contact",
-        address: "ul. Cegielniana 24/3 <br /> 30-404 Cracow",
-        email: "sobeckizbyszko@gmail.com",
-        phone: "+48 600 019 322",
+        title: 'Contact',
+        address: 'ul. Cegielniana 24/3 <br /> 30-404 Cracow',
+        email: 'sobeckizbyszko@gmail.com',
+        phone: '+48 600 019 322',
     },
     skills: {
-        title: "Skills",
+        title: 'Skills',
         list: `<li>HTML5/CSS3 (Sass, Less, Bootstrap)</li>
-        <li>JavaScript/TypeScript (React, React Native, Vue, Socket.io, Node, jQuery, Leaflet, Mobx)</li>
+        <li>JavaScript/TypeScript (React, React Native, Vue, Socket.io, Node, jQuery, Leaflet, Mobx, Next, Astro)</li>
         <li>Module bundlers (Webpack, Parcel, Rollup)</li>
         <li>Version control systems (Github, SVN)</li>
         <li>Databases (MySQL, PostgreSQL, MongoDB)</li>
@@ -28,56 +27,56 @@ export const translations: ITranslations = {
         <li>Basics of Python, PHP, Batch, Shell</li>`,
     },
     languages: {
-        title: "Languages",
+        title: 'Languages',
         list: `<li>Polish - native</li><li>English - B2</li>`,
     },
     experience: {
-        title: "Experience",
+        title: 'Experience',
         firstJob: {
-            company: "Endurance Systems LLC",
-            position: "Frontend Developer",
-            dates: "from July 2022 to October 2022",
+            company: 'Pluscode',
+            position: 'Frontend Developer',
+            dates: 'from July 2023 to now',
             description:
-                "Working with large JSON/GeoJSON files. During my time in this company I used technologies like TypeScript, React 18, Rollup, different map libraries and SVN as version control system. I also wrote a slider at the time, which is available for download on npm.",
+                'Made websites using React and Astro. While working for this company I was also exposed to creating a browser plugin dealing with scraping. I also had the opportunity to work on advanced animations using GSAP.',
         },
         secondJob: {
-            company: "Krakweb",
-            position: "Internship",
-            dates: "November/December 2020",
+            company: 'Endurance Systems LLC',
+            position: 'Frontend Developer',
+            dates: 'from July 2022 to October 2022',
+            description:
+                'Working with large JSON/GeoJSON files. During my time in this company I used technologies like TypeScript, React 18, Rollup, different map libraries and SVN as version control system. I also wrote a slider at the time, which is available for download on npm.',
+        },
+        thirdJob: {
+            company: 'Krakweb',
+            position: 'Internship',
+            dates: 'November/December 2020',
             description:
                 "Creating my own website as a template for clients. To achieve this I used HTML, SCSS, JavaScript, and the company's CMS. The apprenticeship was completed with the highest grade.",
         },
-        thirdJob: {
-            company: "Eurokreator",
-            position: "Internship",
-            dates: "June 2020",
-            description:
-                "Making research regarding MOOC platforms and software used to create webcasts. Furthermore, I learned how to use WordPress and Google API. The apprenticeship was completed with the highest grade.",
-        },
     },
     education: {
-        title: "Education",
-        school: "Technical Highschool of Communications No. 14 in Krakow",
-        dates: "September 2018 - May 2022",
-        firstExam: "EE.08 Exam 90%/100% (theory/practice)",
-        secondExam: "EE.09 Exam 90%/98% (theory/practice)",
-        university: "Cracow University of Technology",
-        universityDates: "September 2022 - now",
+        title: 'Education',
+        school: 'Technical Highschool of Communications No. 14 in Krakow',
+        dates: 'September 2018 - May 2022',
+        firstExam: 'EE.08 Exam 90%/100% (theory/practice)',
+        secondExam: 'EE.09 Exam 90%/98% (theory/practice)',
+        university: 'Cracow University of Technology',
+        universityDates: 'September 2022 - now',
         universityDescription: `Faculty of Applied Computer Science`,
     },
     courses: {
-        title: "Courses and lectures",
+        title: 'Courses and lectures',
         firstCourse: {
-            title: "Developing start-up",
-            description: "Development of my own start-up in the SEED project. Cracow",
+            title: 'Developing start-up',
+            description: 'Development of my own start-up in the SEED project. Cracow',
         },
         secondCourse: {
-            title: "AutoCAD course",
-            description: "AutoCAD 2018 2D course as part of additional classes at school",
+            title: 'AutoCAD course',
+            description: 'AutoCAD 2018 2D course as part of additional classes at school',
         },
         thirdCourse: {
             title: `Erasmus+ Youth Exchange "Can I join you?"`,
-            description: "Taking a part in Erasmus+ project in Store Klaus, Denmark<br /> (31 May - 08 June, 2022)",
+            description: 'Taking a part in Erasmus+ project in Store Klaus, Denmark<br /> (31 May - 08 June, 2022)',
         },
     },
-};
+}
--- src/constants/languages/pl.ts
+++ src/constants/languages/pl.ts
@@ -1,25 +1,24 @@
-import { ITranslations } from "../../interfaces/languages";
-
+import { ITranslations } from '../../interfaces/languages'
 export const translations: ITranslations = {
     overlay: {
-        download: "Pobierz CV",
-        changeLanguage: "Zmień język:",
+        download: 'Pobierz CV',
+        changeLanguage: 'Zmień język:',
     },
     about: {
-        title: "Kim jestem?",
+        title: 'Kim jestem?',
         description:
-            "Jestem ambitnym studentem szukającym nowych wyzwań. Interesuję się elektroniką, psychologią, projektowaniem gier oraz montażem wideo. Dodatkowo jestem osobą bardzo komunikatywną i otwartą, dlatego lubię podróżować, poznawać nowych ludzi i poszerzać swoje horyzonty. Uwielbiam analizować problemy i rozwiązywać je w najlepszy, optymalny sposób.",
+            'Jestem ambitnym studentem szukającym nowych wyzwań. Interesuję się elektroniką, psychologią, projektowaniem gier oraz montażem wideo. Dodatkowo jestem osobą bardzo komunikatywną i otwartą, dlatego lubię podróżować, poznawać nowych ludzi i poszerzać swoje horyzonty. Uwielbiam analizować problemy i rozwiązywać je w najlepszy, optymalny sposób.',
     },
     contact: {
-        title: "Kontakt",
-        address: "ul. Cegielniana 24/3 <br /> 30-404 Kraków",
-        email: "sobeckizbyszko@gmail.com",
-        phone: "+48 600 019 322",
+        title: 'Kontakt',
+        address: 'ul. Cegielniana 24/3 <br /> 30-404 Kraków',
+        email: 'sobeckizbyszko@gmail.com',
+        phone: '+48 600 019 322',
     },
     skills: {
-        title: "Umiejętności",
+        title: 'Umiejętności',
         list: `<li>HTML5/CSS3 (Sass, Less, Bootstrap)</li>
-          <li>JavaScript/TypeScript (React, React Native, Vue, Socket.io, Node, jQuery, Leaflet, Mobx)</li>
+          <li>JavaScript/TypeScript (React, React Native, Vue, Socket.io, Node, jQuery, Leaflet, Mobx, Next, Astro)</li>
           <li>Module bundlers (Webpack, Parcel, Rollup)</li>
           <li>Systemy kontroli wersji (Github, SVN)</li>
           <li>Bazy danych (MySQL, PostgreSQL, MongoDB)</li>
@@ -28,56 +27,56 @@ export const translations: ITranslations = {
           <li>Podstawy języków Python, PHP, Batch, Shell</li>`,
     },
     languages: {
-        title: "Języki",
+        title: 'Języki',
         list: `<li>Polski - ojczysty</li><li>Angielski - B2</li>`,
     },
     experience: {
-        title: "Doświadczenie",
         firstJob: {
-            company: "Endurance Systems LLC",
-            position: "Frontend Developer",
-            dates: "od lipca 2022 do października 2022",
+            company: 'Pluscode',
+            position: 'Frontend Developer',
+            dates: 'od lipca 2023 do teraz',
             description:
-                "Praca z dużymi plikami JSON/GeoJSON. Podczas mojego pobytu w tej firmie korzystałem z takich technologii jak React 18, Rollup, różnych bibliotek do obsługi map i SVN jako systemu kontroli wersji. Napisałem wtedy również slider, który jest udostępniony do pobrania na npm.",
+                "Tworzenie stron internetowych przy użyciu React'a i Astro. Podczas pracy w tej firmie miałem też styczność z tworzeniem wtyczki przeglądarki zajmującej się scrapingiem. Miałem również okazję popracować nad zaawansowanymi animacjami z użyciem GSAP.",
         },
+        title: 'Doświadczenie',
         secondJob: {
-            company: "Krakweb",
-            position: "Praktyki",
-            dates: "Listopad/Grudzień 2020",
+            company: 'Endurance Systems LLC',
+            position: 'Frontend Developer',
+            dates: 'od lipca 2022 do października 2022',
             description:
-                "Stworzenie własnej strony internetowej jako szablonu dla klientów. Do tego celu wykorzystałem HTML, SCSS, JavaScript oraz firmowy CMS. Praktyka zawodowa została zakończona z oceną celującą.",
+                'Praca z dużymi plikami JSON/GeoJSON. Podczas mojego pobytu w tej firmie korzystałem z takich technologii jak React 18, Rollup, różnych bibliotek do obsługi map i SVN jako systemu kontroli wersji. Napisałem wtedy również slider, który jest udostępniony do pobrania na npm.',
         },
         thirdJob: {
-            company: "Eurokreator",
-            position: "Praktyki",
-            dates: "czerwiec 2020",
+            company: 'Krakweb',
+            position: 'Praktyki',
+            dates: 'Listopad/Grudzień 2020',
             description:
-                "Wykonanie badań dotyczących platform MOOC oraz oprogramowania wykorzystywanego do tworzenia webcastów. Ponadto nauczyłem się korzystać z WordPressa oraz Google API. Praktyki zawodowe zostały ukończone z oceną celującą.",
+                'Stworzenie własnej strony internetowej jako szablonu dla klientów. Do tego celu wykorzystałem HTML, SCSS, JavaScript oraz firmowy CMS. Praktyka zawodowa została zakończona z oceną celującą.',
         },
     },
     education: {
-        title: "Edukacja",
-        school: "Technikum Łączności nr 14 w Krakowie",
-        dates: "wrzesień 2018 - maj 2022",
-        firstExam: "Egzamin EE.08 90%/100% (teoria/praktyka)",
-        secondExam: "Egzamin EE.09 90%/98% (teoria/praktyka)",
-        university: "Politechnika Krakowska",
-        universityDates: "wrzesień 2022 - obecnie",
+        title: 'Edukacja',
+        school: 'Technikum Łączności nr 14 w Krakowie',
+        dates: 'wrzesień 2018 - maj 2022',
+        firstExam: 'Egzamin EE.08 90%/100% (teoria/praktyka)',
+        secondExam: 'Egzamin EE.09 90%/98% (teoria/praktyka)',
+        university: 'Politechnika Krakowska',
+        universityDates: 'wrzesień 2022 - obecnie',
         universityDescription: `Studia na kierunku Informatyka Stosowana`,
     },
     courses: {
-        title: "Kursy i szkolenia",
+        title: 'Kursy i szkolenia',
         firstCourse: {
-            title: "Rozwój start-upu",
-            description: "Rozwój własnego start-upu w ramach projektu fundacji SEED Kraków",
+            title: 'Rozwój start-upu',
+            description: 'Rozwój własnego start-upu w ramach projektu fundacji SEED Kraków',
         },
         secondCourse: {
-            title: "Kurs AutoCAD",
-            description: "Kurs AutoCAD 2018 2D w ramach dodatkowych zajęć w szkole",
+            title: 'Kurs AutoCAD',
+            description: 'Kurs AutoCAD 2018 2D w ramach dodatkowych zajęć w szkole',
         },
         thirdCourse: {
             title: `Wymiana młodzieży w ramach programu Erasmus+ "Can I join you?"`,
-            description: "Udział w projekcie Erasmus+ w Store Klaus, Dania <br /> (31 maja - 08 czerwca, 2022)",
+            description: 'Udział w projekcie Erasmus+ w Store Klaus, Dania <br /> (31 maja - 08 czerwca, 2022)',
         },
     },
-};
+}
